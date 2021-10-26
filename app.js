3c0590a60ce32e8c33fbdf4a4200fb05079139b9
satywan <satywankumar646@gmail.com>
2021-10-26 04:30:00 -0900

    added some code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const batchRoute = require('./api/route/batch');
 const studentRoute = require('./api/route/student');
 const facultyRoute = require('./api/route/faculty');
@@ -11,7 +12,8 @@ const { urlencoded, json } = require('body-parser');
 const cors = require('cors');
 const fileUpload = require('express-fileupload');
 
-mongoose.connect('mongodb+srv://sribstech:sribstech@123@sribstech.gpk49.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true});
+
+mongoose.connect('mongodb+srv://.gpk49.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true});
 
 mongoose.connection.on('error',err=>{
   console.log('connection failed');
@@ -35,11 +37,16 @@ app.use('/student',studentRoute);
 app.use('/faculty',facultyRoute);
 app.use('/payment',paymantRoute);
 app.use('/user',userRoute);
+app.use(express.static(__dirname+'www'));
 
-app.use((req,res,next)=>{
-  res.status(404).json({
-    error:'bad request'
-  })
+// app.use((req,res,next)=>{
+//   res.status(404).json({
+//     error:'bad request'
+//   })
+// })
+
+app.get('*',(req,res,next)=>{
+  res.sendFile(path.join(__dirname + '/www/index.html'));
 })
 
 
